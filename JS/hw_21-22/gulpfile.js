ec9efb74a31af8562c74835023bebb104de9c215
andreusfreeman <andriy.natsyk@gmail.com>
2016-07-25 22:35:10 +0300

    changed structure project

---
+++ JS/hw_21-22/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sass = require('gulp-sass');
+
+gulp.task('babel', () => {
+	return gulp.src('src/js/script.js')
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('src/styles/*.scss', ['sass']);
+});
